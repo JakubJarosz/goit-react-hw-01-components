7d5f35ad2b92d75805bc9fe49f9fc5ead636f3c5
JakubJarosz <jakubtm165@gmail.com>
2022-09-02 16:26:12 +0200

    Updates

---
+++ precache-manifest.16bb33655e42c81b9267148f8e4fd275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b1a29e9b8d9a49f0007",
+    "url": "/goit-react-hw-01-components/static/css/main.dfed0f90.chunk.css"
+  },
+  {
+    "revision": "2b1a29e9b8d9a49f0007",
+    "url": "/goit-react-hw-01-components/static/js/main.2b1a29e9.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/goit-react-hw-01-components/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "cedf912a48a66f966d16",
+    "url": "/goit-react-hw-01-components/static/js/2.cedf912a.chunk.js"
+  },
+  {
+    "revision": "953574b94202d560950d",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.953574b9.js"
+  },
+  {
+    "revision": "367dab719c99a51c302e626eb3f392ea",
+    "url": "/goit-react-hw-01-components/index.html"
+  }
+];
